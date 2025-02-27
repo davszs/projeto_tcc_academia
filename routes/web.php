4bab8ef618e6c67162e6e4c4769f4e152930e294
davszs <dssantos4409@gmail.com>
2025-02-26 21:42:23 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+route::get('/login', function() {
+    echo "login";
+});
+
+route::get('/rec_senha', function() {
+    echo "recuperar senha";
+});
+
+route::get('/painel_controle', function() {
+    echo "painel de controle";
+});
+
+route::get('/funcionarios', function() {
+    echo "funcionarios";
+});
+
+route::get('/planejamento_aulas', function() {
+    echo "planejamento de aulas";
+});
+
+route::get('/config_geral', function() {
+    echo "configurações gerais";
+});
