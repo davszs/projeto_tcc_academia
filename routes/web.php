093ac063db3ee90df445a2d8662574c9e38966e7
davszs <dssantos4409@gmail.com>
2025-02-27 13:02:11 -0300

    Criação dos Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\PrincipalController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PrincipalController::class, 'principal']);
 
 
 route::get('/login', function() {
