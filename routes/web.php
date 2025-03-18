e3236dc9ac0644140dd331fad246620adc93fc51
davszs <dssantos4409@gmail.com>
2025-03-18 14:16:38 -0300

    rota e controller para cadastro de usuário

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,35 @@
 <?php
 
 use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PrincipalController::class, 'principal']);
 
+route::get('/usuarios/cadastrar',[UsuariosController::class,'showRegisterForm']);
+route::post('/usuarios', [UsuariosController::class]);
 
-route::get('/login', function() {
-    echo "login";
-});
+    // route::post(
+    //     '/login', 
+    //     [LoginController::class]);
+    
+// route::get('/rec_senha', function() {
+//     echo "recuperar senha";
+// });
 
-route::get('/rec_senha', function() {
-    echo "recuperar senha";
-});
+// route::get('/painel_controle', function() {
+//     echo "painel de controle";
+// });
 
-route::get('/painel_controle', function() {
-    echo "painel de controle";
-});
+// route::get('/funcionarios', function() {
+//     echo "funcionarios";
+// });
 
-route::get('/funcionarios', function() {
-    echo "funcionarios";
-});
+// route::get('/planejamento_aulas', function() {
+//     echo "planejamento de aulas";
+// });
 
-route::get('/planejamento_aulas', function() {
-    echo "planejamento de aulas";
-});
-
-route::get('/config_geral', function() {
-    echo "configurações gerais";
-});
+// route::get('/config_geral', function() {
+//     echo "configurações gerais";
+// });
